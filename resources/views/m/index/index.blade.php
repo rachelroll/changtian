0c986fc9c75f21e0f197d0f79c8b13f883ff0aab
rachelroll <hai30@163.com>
2019-06-16 01:30:02 +0800

    update(source html)

--- resources/views/m/index/index.blade.php
+++ resources/views/m/index/index.blade.php
@@ -1,1 +1,110 @@
-resources/views/m/index/index.blade.php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.bootcss.com/font-awesome/5.8.2/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.bootcss.com/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+
+    <script src="https://cdn.bootcss.com/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdn.bootcss.com/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+    <title>溯源查询</title>
+</head>
+<body>
+<div class="alert alert-success" role="alert">
+    <div class="row">
+        <h5 class="alert-heading d-block mx-auto">溯源码编号:NO.12345666666</h5>
+    </div>
+
+</div>
+<div class="row">
+    <div class="d-block mx-auto">
+        <div class="text-success">查询记录</div>
+    </div>
+</div>
+<br>
+<div class="container">
+    <div class="row">
+        <div class="d-block mx-auto">
+            <img src="https://via.placeholder.com/50X50/D95353/ccc?text=ross" class="rounded" alt=""/>
+            <img src="https://via.placeholder.com/50X50/D95353/ccc?text=ross" class="rounded" alt=""/>
+            <img src="https://via.placeholder.com/50X50/D95353/ccc?text=ross" class="rounded" alt=""/>
+            <img src="https://via.placeholder.com/50X50/D95353/ccc?text=ross" class="rounded" alt=""/>
+            <img src="https://via.placeholder.com/50X50/D95353/ccc?text=ross" class="rounded" alt=""/>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <hr>
+    <div class="row">
+        <div class="d-block mx-auto">
+            <div class="text-success">产品信息</div>
+        </div>
+    </div>
+    <br>
+    <div class="container">
+        <div class="row">
+            <ul>
+                <li>品名:新鲜西瓜🍉</li>
+                <li>品名:新鲜西瓜🍉</li>
+                <li>品名:新鲜西瓜🍉</li>
+                <li>品名:新鲜西瓜🍉</li>
+                <li>品名:新鲜西瓜🍉</li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <hr>
+    <div class="row">
+        <div class="d-block mx-auto">
+            <div class="text-success">追溯信息</div>
+        </div>
+    </div>
+    <br>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <i class="far fa-clock" style="font-size: 32px;"></i>
+                <div>生产时间: </div>
+                <div>2018-11-11 </div>
+            </div>
+            <div class="col">
+                <i class="fa fa-clock" style="font-size: 32px;"></i>
+                <div>入库时间: </div>
+                <div>2018-11-11 </div>
+            </div>
+            <div class="col">
+                <i class="fas fa-hourglass-start" style="font-size: 32px;"></i>
+                <div>下单时间: </div>
+                <div>2018-11-11 </div>
+            </div>
+        </div>
+        <br>    
+        <div class="row">
+            <div class="col">
+                <i class="fas fa-outdent" style="font-size: 32px;"></i>
+                <div>出库时间: </div>
+                <div>2018-11-11 </div>
+            </div>
+            <div class="col">
+                <i class="fas fa-truck" style="font-size: 32px;"></i>
+                <div>发货时间: </div>
+                <div>2018-11-11 </div>
+            </div>
+            <div class="col">
+                <i class="fas fa-satellite-dish" style="font-size: 32px;"></i>
+                <div>收货时间: </div>
+                <div>2018-11-11 </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
