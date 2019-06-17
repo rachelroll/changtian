18d525c91d33e205e5b7652685aa633215a7fbac
rachelroll <hai30@163.com>
2019-06-17 23:02:02 +0800

    update(m.index)

--- resources/views/m/index/index.blade.php
+++ resources/views/m/index/index.blade.php
@@ -22,18 +22,18 @@
 </div>
 <div class="row">
     <div class="d-block mx-auto">
-        <div class="text-success">查询记录</div>
+        <div class="text-info">查询记录</div>
     </div>
 </div>
 <br>
 <div class="container">
     <div class="row">
         <div class="d-block mx-auto">
-            <img src="https://via.placeholder.com/50X50/D95353/ccc?text=ross" class="rounded" alt=""/>
-            <img src="https://via.placeholder.com/50X50/D95353/ccc?text=ross" class="rounded" alt=""/>
-            <img src="https://via.placeholder.com/50X50/D95353/ccc?text=ross" class="rounded" alt=""/>
-            <img src="https://via.placeholder.com/50X50/D95353/ccc?text=ross" class="rounded" alt=""/>
-            <img src="https://via.placeholder.com/50X50/D95353/ccc?text=ross" class="rounded" alt=""/>
+            <img src="https://via.placeholder.com/50X50/D95353/ccc?text=ross" class="rounded-circle" alt=""/>
+            <img src="https://via.placeholder.com/50X50/D95353/ccc?text=ross" class="rounded-circle" alt=""/>
+            <img src="https://via.placeholder.com/50X50/D95353/ccc?text=ross" class="rounded-circle" alt=""/>
+            <img src="https://via.placeholder.com/50X50/D95353/ccc?text=ross" class="rounded-circle" alt=""/>
+            <img src="https://via.placeholder.com/50X50/D95353/ccc?text=ross" class="rounded-circle" alt=""/>
         </div>
     </div>
 </div>
@@ -42,20 +42,17 @@
     <hr>
     <div class="row">
         <div class="d-block mx-auto">
-            <div class="text-success">产品信息</div>
+            <div class="text-info">产品信息</div>
         </div>
     </div>
-    <br>
-    <div class="container">
-        <div class="row">
-            <ul>
-                <li>品名:新鲜西瓜🍉</li>
-                <li>品名:新鲜西瓜🍉</li>
-                <li>品名:新鲜西瓜🍉</li>
-                <li>品名:新鲜西瓜🍉</li>
-                <li>品名:新鲜西瓜🍉</li>
-            </ul>
-        </div>
+    <div class="row">
+        <ul class=" list-group-flush">
+            <li class="list-group-item">品名:新鲜西瓜ssdfsdf </li>
+            <li class="list-group-item">品名:新鲜西瓜🍉</li>
+            <li class="list-group-item">品名:新鲜西瓜🍉</li>
+            <li class="list-group-item">品名:新鲜西瓜🍉</li>
+            <li class="list-group-item">品名:新鲜西瓜🍉</li>
+        </ul>
     </div>
 </div>
 
@@ -63,47 +60,63 @@
     <hr>
     <div class="row">
         <div class="d-block mx-auto">
-            <div class="text-success">追溯信息</div>
+            <div class="text-info">追溯信息</div>
+        </div>
+    </div>
+    <div class="row p-3">
+        <div class="col">
+            <i class="far fa-clock pl-3 text-info" style="font-size: 32px;"></i>
+            <div >生产时间:</div>
+            <div >2018-11-11</div>
+        </div>
+        <div class="col">
+            <i class="fa fa-clock pl-3 text-info" style="font-size: 32px;"></i>
+            <div>入库时间:</div>
+            <div>2018-11-11</div>
+        </div>
+        <div class="col">
+            <i class="fas fa-hourglass-start pl-3 text-info" style="font-size: 32px;"></i>
+            <div>下单时间:</div>
+            <div>2018-11-11</div>
         </div>
     </div>
     <br>
-    <div class="container">
-        <div class="row">
-            <div class="col">
-                <i class="far fa-clock" style="font-size: 32px;"></i>
-                <div>生产时间: </div>
-                <div>2018-11-11 </div>
-            </div>
-            <div class="col">
-                <i class="fa fa-clock" style="font-size: 32px;"></i>
-                <div>入库时间: </div>
-                <div>2018-11-11 </div>
-            </div>
-            <div class="col">
-                <i class="fas fa-hourglass-start" style="font-size: 32px;"></i>
-                <div>下单时间: </div>
-                <div>2018-11-11 </div>
-            </div>
+    <div class="row p-3">
+        <div class="col">
+            <i class="fas fa-outdent pl-3 text-info" style="font-size: 32px;"></i>
+            <div>出库时间:</div>
+            <div>2018-11-11</div>
         </div>
-        <br>    
-        <div class="row">
-            <div class="col">
-                <i class="fas fa-outdent" style="font-size: 32px;"></i>
-                <div>出库时间: </div>
-                <div>2018-11-11 </div>
-            </div>
-            <div class="col">
-                <i class="fas fa-truck" style="font-size: 32px;"></i>
-                <div>发货时间: </div>
-                <div>2018-11-11 </div>
-            </div>
-            <div class="col">
-                <i class="fas fa-satellite-dish" style="font-size: 32px;"></i>
-                <div>收货时间: </div>
-                <div>2018-11-11 </div>
-            </div>
+        <div class="col">
+            <i class="fas fa-truck pl-3 text-info" style="font-size: 32px;"></i>
+            <div>发货时间:</div>
+            <div>2018-11-11</div>
+        </div>
+        <div class="col">
+            <i class="fab fa-get-pocket pl-3 text-info" style="font-size: 32px;"></i>
+            <div>收货时间:</div>
+            <div>2018-11-11</div>
         </div>
     </div>
+</div>
+
+{{--企业信息--}}
+<div class="container">
+    <hr>
+    <div class="row">
+        <div class="d-block mx-auto">
+            <div class="text-info">企业信息</div>
+        </div>
+    </div>
+    <div class="row p-3">
+        <div class="col">
+            <div >供应商: 宁夏昌田农业发展有限公司</div>
+            <div >热线: 400-666-8683</div>
+            <div >企业地址: 山东青岛</div>
+        </div>
+    </div>
+    <br>
+    <br>
 </div>
 
 </body>
