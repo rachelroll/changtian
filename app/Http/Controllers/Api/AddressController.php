2ad8a049def23c8d4ddaf2b046849c26d1c7743e
rachelroll <hai30@163.com>
2019-07-08 17:55:14 +0800

    update(address)

--- app/Http/Controllers/Api/AddressController.php
+++ app/Http/Controllers/Api/AddressController.php
@@ -3,22 +3,24 @@
 namespace App\Http\Controllers\api;
 
 use App\Address;
+use App\ChinaArea;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Resources\Address as AddressResource;
 
 class AddressController extends Controller
 {
+
     // 地址列表
     public function index()
     {
         $addresses = Address::all();
         if ($addresses) {
             return AddressResource::collection($addresses);
-        } else{
+        } else {
             return [
                 'code' => 700,
-                'msg' => '没有地址列表'
+                'msg'  => '没有地址列表',
             ];
         }
     }
@@ -34,27 +36,33 @@ class AddressController extends Controller
         $phone = $request->mobile;
         $code = $request->code;
         $isDefault = $request->isDefalut;
+        $provinceStr = ChinaArea::where('code', substr($provinceId,0,6))->first()->name;
+        $areaStr = ChinaArea::where('code', substr($cityId,0,6))->first()->name;
+        $cityStr = ChinaArea::where('code', substr($districtId,0,6))->first()->name;
 
         $bool = Address::create([
-            'provinceId' => $provinceId,
-            'cityId' => $cityId,
-            'districtId' => $districtId,
-            'address' => $address,
+            'provinceId'   => $provinceId,
+            'cityId'       => $cityId,
+            'districtId'   => $districtId,
+            'address'      => $address,
             'contact_name' => $contact_name,
-            'phone' => $phone,
-            'code' => $code,
-            'isDefault' => $isDefault
+            'phone'        => $phone,
+            'code'         => $code,
+            'isDefault'    => $isDefault,
+            'provinceStr'  => $provinceStr,
+            'areaStr'      => $areaStr,
+            'cityStr'      => $cityStr,
         ]);
 
         if ($bool) {
             return [
                 'code' => 0,
-                'msg' => 'success'
+                'msg'  => 'success',
             ];
         } else {
             return [
                 'code' => 202,
-                'msg' => 'fail'
+                'msg'  => 'fail',
             ];
         }
     }
@@ -71,27 +79,33 @@ class AddressController extends Controller
         $phone = $request->mobile;
         $code = $request->code;
 
-        $bool = Address::where('id', $id)
-            ->update([
-            'provinceId' => $provinceId,
-            'cityId' => $cityId,
-            'districtId' => $districtId,
-            'address' => $address,
-            'contact_name' => $contact_name,
-            'phone' => $phone,
-            'code' => $code,
-            'isDefault' => ''
-        ]);
+        $provinceStr = ChinaArea::where('code', substr($provinceId,0,6))->first()->name;
+        $areaStr = ChinaArea::where('code', substr($cityId,0,6))->first()->name;
+        $cityStr = ChinaArea::where('code', substr($districtId,0,6))->first()->name;
+
+        $bool = Address::where('id', $id)->update([
+                'provinceId'   => $provinceId,
+                'cityId'       => $cityId,
+                'districtId'   => $districtId,
+                'address'      => $address,
+                'contact_name' => $contact_name,
+                'phone'        => $phone,
+                'code'         => $code,
+                'isDefault'    => '',
+                'provinceStr'  => $provinceStr,
+                'areaStr'      => $areaStr,
+                'cityStr'      => $cityStr,
+            ]);
 
         if ($bool) {
             return [
                 'code' => 0,
-                'msg' => 'success'
+                'msg'  => 'success',
             ];
         } else {
             return [
                 'code' => 202,
-                'msg' => 'fail'
+                'msg'  => 'fail',
             ];
         }
     }
@@ -105,8 +119,8 @@ class AddressController extends Controller
             return new AddressResource($address);
         } else {
             return [
-                'code' =>400,
-                'msg' => '尚无默认地址',
+                'code' => 400,
+                'msg'  => '尚无默认地址',
             ];
         }
     }
