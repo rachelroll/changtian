881ccccdc09fe4c48f6eee96b67dad03399a1b42
rachelroll <hai30@163.com>
2019-07-16 23:58:30 +0800

    update(goods source_video)

--- app/Http/Resources/Good.php
+++ app/Http/Resources/Good.php
@@ -32,7 +32,7 @@ class Good extends JsonResource
                     'size' => $this->size, // 规格与包装
                     'is_source' => $this->is_source, // 是否溯源
                     'source_content' => $this->source_content, // 是否溯源
-                    'source_video' => config('filesystems.disks.oss.cdnDomain') . '/' . $this->source_video, // 是否溯源
+                    'source_video' => $this->source_video ? config('filesystems.disks.oss.cdnDomain') . '/' . $this->source_video : null, // 是否溯源
                     'videoId' => $this->videoId // 视频 ID
                 ],
                 'category' => [
